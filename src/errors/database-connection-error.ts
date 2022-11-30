24806660a49d13c8de0f483de6ae54ec29d4a867
Michael <lemichael1215@gmail.com>
2022-11-30 21:29:20 +0300

    add error-handler and response normalization strategies

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,20 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = "Error connecting to database";
+
+  constructor() {
+    super("Error connecting to db");
+
+    //Only because we are extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serialzeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
